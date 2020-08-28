505440352cd994b11024bbf6d7f01ea5fd48f1d4
Xinyuan Huang <huangxy0101@gmail.com>
2020-08-27 17:01:12 -0700

    Refactor docker runner
    This refactors docker runner to comply with the runner spec.

--- runners/mlcommons_box_docker/mlcommons_box_docker/__main__.py
+++ runners/mlcommons_box_docker/mlcommons_box_docker/__main__.py
@@ -2,6 +2,8 @@ import os
 import click
 from mlcommons_box import parse   # Do not remove (it registers schemas on import)
 from mlcommons_box.common import mlbox_metadata
+from mlcommons_box.runner import utils as runner_utils
+
 from mlcommons_box_docker import metadata
 from mlcommons_box_docker.docker_run import DockerRun
 
@@ -18,11 +20,12 @@ def cli():
 @click.option('--mlbox', required=True, type=click.Path(exists=True), help='Path to MLBox directory.')
 @click.option('--platform', required=True, type=click.Path(exists=True), help='Path to MLBox Platform definition file.')
 def configure(mlbox: str, platform: str):
-    mlbox: mlbox_metadata.MLBox = mlbox_metadata.MLBox(path=mlbox)
-    mlbox.platform = metadata.DockerPlatform(path=platform, mlbox=mlbox)
+    mlbox_root = mlbox
+    mlbox: mlbox_metadata.MLBox = mlbox_metadata.MLBox(path=mlbox_root)
+    runner_config = runner_utils.build_runner_config(mlbox_root, platform, 'configure', None)
     print(mlbox)
 
-    runner = DockerRun(mlbox)
+    runner = DockerRun(mlbox, runner_config)
     runner.configure()
 
 
@@ -31,13 +34,14 @@ def configure(mlbox: str, platform: str):
 @click.option('--platform', required=True, type=click.Path(exists=True), help='Path to MLBox Platform definition file.')
 @click.option('--task', required=True, type=click.Path(exists=True), help='Path to MLBox Task definition file.')
 def run(mlbox: str, platform: str, task: str):
-    mlbox: mlbox_metadata.MLBox = mlbox_metadata.MLBox(path=mlbox)
-    mlbox.platform = metadata.DockerPlatform(path=platform, mlbox=mlbox)
+    mlbox_root = mlbox
+    mlbox: mlbox_metadata.MLBox = mlbox_metadata.MLBox(path=mlbox_root)
     mlbox.invoke = mlbox_metadata.MLBoxInvoke(task)
     mlbox.task = mlbox_metadata.MLBoxTask(os.path.join(mlbox.tasks_path, f'{mlbox.invoke.task_name}.yaml'))
+    runner_config = runner_utils.build_runner_config(mlbox_root, platform, 'run', mlbox.invoke.task_name)
     print(mlbox)
 
-    runner = DockerRun(mlbox)
+    runner = DockerRun(mlbox, runner_config)
     runner.run()
 
 
